2ee964bc88f7103c462a4d493604df4992f8cc9e
Miguel Pedrosa <pedrosa.miguel@gmail.com>
2021-08-25 15:30:37 +0100

    changes to test workflow

--- src/App.svelte
+++ src/App.svelte
@@ -41,7 +41,7 @@
 <div class="App">
   <header class="App-header">
     <img src="/logo.svg" class="App-logo" alt="logo" />
-    <p>Page has been open for <code>{count}</code> seconds.</p>
+    <p>Page has been open for at least <code>{count}</code> seconds.</p>
     <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
       Learn Svelte
     </a>
